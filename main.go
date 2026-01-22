7b74fa425b7bee422e56fe759de14e96358cc25a
Santiago Urdaneta <santurdaneta@gmail.com>
2026-01-21 20:26:40 -0500

    small fixes

--- main.go
+++ main.go
@@ -74,7 +74,7 @@ func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-Frame-Options", "DENY")
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
-		w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self' https://unpkg.com; style-src 'self' 'unsafe-inline';")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self' https://unpkg.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:;")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 
 		// Rate Limiting
